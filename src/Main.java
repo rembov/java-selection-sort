95f26196fe3c523a0b5aa11f420373dec18bca59
rembov <95583621+rembov@users.noreply.github.com>
2024-07-22 00:02:34 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -47,18 +47,18 @@ public class Main {
     }
 
     public static void sort(int[] array) {
-        for (int i = 0; i < array.length; i++) {    // i - номер текущего шага
+        for (int i = 0; i < array.length; i++) {   
             int pos = i;
             int min = array[i];
-            // цикл выбора наименьшего элемента
+          
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < min) {
-                    pos = j;    // pos - индекс наименьшего элемента
+                    pos = j;    
                     min = array[j];
                 }
             }
             array[pos] = array[i];
-            array[i] = min;    // меняем местами наименьший с array[i]
+            array[i] = min;    
         }
     }
 
